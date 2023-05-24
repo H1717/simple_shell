0ea41bddc620065e91a2d3a4d0f7672a97a8fdb7
myUsername <myemail@gmail.com>
2023-05-24 09:27:44 -0700

    realloc.c

--- realloc.c
+++ realloc.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
-  * _memset - fils memory with a constant byte
-  * @s: the pointer to the memory area
-  * @b: the byte to fill *s with
-  * @n: the amount of bytes to be filled
-  * Return: (s) a pointer to the memory area s
-  */
+ ** _memset - fils memory with a constant byte
+ * @s: the pointer to the memory area
+ * @b: the byte to fill *s with
+ * @n: the amount of bytes to be filled
+ * Return: (s) a pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
@@ -22,7 +22,7 @@ char *_memset(char *s, char b, unsigned int n)
   */
 void ffree(char **p)
 {
-	char **p = pp;
+	char **a = pp;
 
 	if (!pp)
 		return;
@@ -32,13 +32,13 @@ void ffree(char **p)
 }
 
 /**
-  *_reallpc - reallocates a block of memory
-  * @ptr: pointer to previous malloc'ated block
-  * @old_size: byte size of previous block
-  * @new_size: bute size of new block
-  *
-  * Return: pointer to da ol'block nameen.
-  */
+ *_reallpc - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_size: byte size of previous block
+ * @new_size: bute size of new block
+ *
+ * Return: pointer to da ol'block nameen.
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
