50db3255aed55ad239910ae13fb957fb03aa1c91
myUsername <myemail@gmail.com>
2023-05-24 09:59:55 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -18,31 +18,31 @@
 #define BUF_FLUSH -1
 
 /* for command chaining */
-#define CMD_NORM 0
-#define CMD_OR 1
-#define CMD_AND 2
-#define CMD_CHAIN 3
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND 	2
+#define CMD_CHAIN 	3
 
 /* for convert_numbre() */
-#define CONVERT_LOZERCASE  1
-#define CONVERT_UNSIGNED 2
+#define CONVERT_LOWERCASE 	1
+#define CONVERT_UNSIGNED 	2
 
 /* 1 if using system getline() */
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 
-#define HIST_FILE  ".simple_shell_history"
-#define HIST_ MAX  4096
+#define HIST_FILE	".simple_shell_history"
+#define HIST_ MAX	4096
 
 extern char **environ;
 
 
 /**
-  * struct liststr - singly linked list
-  * @num: the number field
-  * @str: a string
-  * @next: points to the next node
-  */
+ * struct liststr - singly linked list
+ * @num: the number field
+ * @str: a string
+ * @next: points to the next node
+ */
 typedef struct liststr
 {
 	int num;
@@ -51,27 +51,27 @@ typedef struct liststr
 } list_t
 
 /**
-  * struct passinfo - contains pseudo-arguements to pass into a function,
-  * allowing uniform prototype for function pointer struct
-  * @arg: a string generated from getline containing arguments
-  * @argv: an array of string generated from arg
-  * @path: a string path for the current command
-  * @argc: the argument count
-  * @line_count: the error count
-  * @err_num: the error code for exit()s
-  * @linecount_flag: if on count this line of input
-  * @fname: the program filename
-  * @env: linked list local copy of environ
-  * @environ: custom modified copy of environ from LL env
-  * @history: the history node
-  * @alias: the alias node
-  * @env_changed: on if environ was changed
-  * @status: the return status of the last exec'd command
-  * @cmd_buf: address of pointer to cmd_buf, on if chaining
-  * @cmd_buf_type: CMD_type ||, &&, ;
-  * @readfd: the fd from which to read line input
-  * @histcount: the history line number count
-  */
+ * struct passinfo - contains pseudo-arguements to pass into a function,
+ * 		 allowing uniform prototype for function pointer struct
+ * @arg: a string generated from getline containing arguments
+ * @argv: an array of string generated from arg
+ * @path: a string path for the current command
+ * @argc: the argument count
+ * @line_count: the error count
+ * @err_num: the error code for exit()s
+ * @linecount_flag: if on count this line of input
+ * @fname: the program filename
+ * @env: linked list local copy of environ
+ * @environ: custom modified copy of environ from LL env
+ * @history: the history node
+ * @alias: the alias node
+ * @env_changed: on if environ was changed
+ * @status: the return status of the last exec'd command
+ * @cmd_buf: address of pointer to cmd_buf, on if chaining
+ * @cmd_buf_type: CMD_type ||, &&, ;
+ * @readfd: the fd from which to read line input
+ * @histcount: the history line number count
+ */
 typedef struct passinfo
 {
 	char *arg;
@@ -96,7 +96,8 @@ typedef struct passinfo
 } info_t
 
 #define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \0, 0, 0}
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
 
 /**
  *struct builtin - contains a builtin string and related function
@@ -207,7 +208,7 @@ int _setenv(info_t *, char *, char *);
 char *get_history_file(info_t *info);
 int write_history(info_t *info);
 int read_history(info_t *info);
-int build_history(info_t *info, char *buf, int linecount);
+int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 
 /* toem_lists.c */
