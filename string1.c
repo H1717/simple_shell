e4b794c5c5ae7a4b390f67bf1435d56a9d29c97e
myUsername <myemail@gmail.com>
2023-05-24 10:46:57 -0700

    string1.c

--- string1.c
+++ string1.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
-  * _strcpy - copies a string
-  * @dest: the destination
-  * @src: the source
-  *
-  * Return: pointer to destination
-  */
+ * _strcpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: pointer to destination
+ */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
@@ -23,11 +23,11 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
-  * _strdup - duplicates a string
-  * @str: the string to duplicated
-  *
-  * Return: pointer to the duplicated string
-  */
+ * _strdup - duplicates a string
+ * @str: the string to duplicated
+ *
+ * Return: pointer to the duplicated string
+ */
 char *_strdup(const char *str)
 {
 	int lenght = 0;
@@ -38,7 +38,6 @@ char *_strdup(const char *str)
 	while (*str++)
 		lenght++;
 	ret = malloc(sizeof(char) * (lenght + 1));
-
 	if (!ret)
 		return (NULL);
 	for (lenght++; lenght--;)
@@ -47,11 +46,11 @@ char *_strdup(const char *str)
 }
 
 /**
-  * _puts - prints an input string
-  * @str: the string to be printed
-  *
-  * Return: Nothing
-  */
+ * _puts - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
 void _puts(char *str)
 {
 	int i = 0;
@@ -66,12 +65,12 @@ void _puts(char *str)
 }
 
 /**
-  * _putchar - writes the character c to stdout
-  * @c: The character to print
-  *
-  * Return: On success 1.
-  * On error, -1 is returned, and errno is set appropriately.
-  */
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
 	static int i;
