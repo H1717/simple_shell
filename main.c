cdb9daf4f148668f3f361753e650b02b2b002ea1
myUsername <myemail@gmail.com>
2023-05-24 08:59:17 -0700

    main.c

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include "shell.h"
 
 /**
-  * main - entry point
-  * @ac: arg count
-  * @av: arg vector
-  *
-  * Return: 0 on success, 1 on error
-  */
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INT };
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
+		"add $3, %0"
+		: "=r" (fd)
+		:"r" (fd));
+
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
