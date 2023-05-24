8af46c9645e253d834ea6be868c58c063a262209
myUsername <myemail@gmail.com>
2023-05-24 08:43:06 -0700

    lists.c

--- lists.c
+++ lists.c
@@ -1,14 +1,14 @@
 #include "shell.h"
 
 /**
-  * add_node - adds a node to the start of the list
-  * @head: address of pointer to head node
-  * @str: str field of node
-  * @num: node index used by history
-  *
-  * Return: size of list
-  */
-list_t  *add_node(list_t **head, const char *str, int num)
+ * add_node - adds a node to the start of the list
+ * @head: address of pointer to head node
+ * @str: str field of node
+ * @num: node index used by history
+ *
+ * Return: size of list
+ */
+list_t *add_node(list_t **head, const char *str, int num)
 {
 	list_t *new_head;
 
@@ -32,6 +32,7 @@ list_t  *add_node(list_t **head, const char *str, int num)
 	*head = new_head;
 	return (new_head);
 }
+
 /**
   * add_node_end - adds a node to the end of the list
   * @head: address  of pointer to head node
@@ -74,10 +75,11 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 }
 
 /**
-  * print_list_str - prints only the str element of a list_t linked list
-  * @h: pointer to first node
-  * * Return: size of list
-  */
+ * print_list_str - prints only the str element of a list_t linked list
+ * @h: pointer to first node
+ * 
+ * Return: size of list
+ */
 size_t print_list_str(const list_t *h)
 {
 	size_t i = 0;
@@ -93,12 +95,12 @@ size_t print_list_str(const list_t *h)
 }
 
 /**
-  * delete_node_at_index - deletes node at given index
-  * @head: adrress of pointer to first node
-  * @index: index of node to delete
-  *
-  * Return: 1 on success, 0 on failure
-  */
+ * delete_node_at_index - deletes node at given index
+ * @head: adrress of pointer to first node
+ * @index: index of node to delete
+ *
+ * Return: 1 on success, 0 on failure
+ */
 int delete_node_at_index(list_t **head, unsigned int index)
 {
 	list_t *node, *prev_node;
