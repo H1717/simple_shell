4073fbb64c773fe45e03db4b05164e800761c89b
myUsername <myemail@gmail.com>
2023-05-24 05:28:24 -0700

    environ.c

--- environ.c
+++ environ.c
@@ -1,23 +1,25 @@
 #include "shell.h"
 
 /**
-  * _myenv - prints the current environment
-  * @info: Structure containing potential arguments. Used to maintain
-  *	constant function prototype.
-  * Return: Always 0
-  */
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
 int _myenv(info_t *info)
 {
-			print_list_str(info->env);
-			return (0);
+	print_list_str(info->env);
+	return (0);
 }
 
 /**
-  * _getenv - gets the value of n environ variable
-  * @info: Structure containing potential arguments. Used to maintain
-  *	constant fuction prototype.
-  * Return: Always 0
-  */
+ * _getenv - gets the value of n environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return:the value
+ */
+char *_getenv(info_t *info, const char *name)
 {
 	list_t *node = info->env;
 	char *p;
@@ -72,11 +74,11 @@ int _myunsetenv(info_t *info)
 }
 
 /**
-  * populate_env_list - populates env linked list
-  * @info: Structure containing potential arguments. Used to maintain
-  *	constant function prototype.
-  * Return: Always 0
-  */
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *	    constant function prototype.
+ * Return: Always 0
+ */
 int populate_env_list(info_t *info)
 {
 	list_t *node = NULL;
